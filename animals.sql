6cda220460fd8feabbd127153bbd54729c590324
Saqer <ahmadsaqer95@outloo.com>
2021-05-04 19:15:21 +0200

    modify create update delete animal and create animals.sql, thumbnails.sql

---
+++ animals.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS animals;
+CREATE TABLE "animals" (
+    "id" VARCHAR(255) PRIMARY KEY,
+    "name" VARCHAR(255),
+    "latinName" VARCHAR(255),
+    "idVideo" VARCHAR(255),
+    "img" VARCHAR(255)
+);
+INSERT INTO "animals" VALUES
+    ('1UlRpfWybzyjuKseS85arg','Sassy Squirrels','Tamiasciurus hudsonicus','1XsUjY5Z8fk','//images.ctfassets.net/utnfcxl3mjrn/50p1o2nZkNiQZYZGs1308H/48e8b02fa2c1f0391ac3c6f343a2c28c/276650b7-nutty-shopper-t-501069.jpg?w=400&h=300&fit=fill'),
+    ('AUwWYbgfHlLst2DAWJsSV','Courages Chimpanzees','Pan troglodytes','OsoNKlyFtpI','//images.ctfassets.net/utnfcxl3mjrn/ao0RZ0qsh0tIpJOtwiexA/32649a1762d36166b636599656aaa37b/2q82IrV6a2mB9TZmmepYii.jpg?w=400&h=300&fit=fill'),
+    ('3oim0otIL8i81QCEMAUnQG','Goofy Guinea pigs','Cavia porcellus','o0vhplsjmow','//images.ctfassets.net/utnfcxl3mjrn/5EQ8FE3kYsk82Zgnd0gsN5/e32c84a8ad68c8335f336f3f7dcd1350/shutterstock_173581781.jpg?w=400&h=300&fit=fill'),
+    ('3figy4GBMZIHLgiYcl9WuG','Friendly Foxes','Vulpes vulpes','1yAEp5IrrIE','//images.ctfassets.net/utnfcxl3mjrn/2peaP0j7JVxKjxmgHxKQB8/6a1239f1e16e2f07968a1192052217fb/ew3uh6rvq3b11.jpg?w=400&h=300&fit=fill'),
+    ('53ercI899LpCOhJM6FnMRf','Slow Sloths','Folivora','Aeij-oc-18s','//images.ctfassets.net/utnfcxl3mjrn/55716WhUeAJe2retma0Z4M/e8e0725c7858db1865d1cccd948d4740/2fd3ee55ad3089fa62010ce48d296952.jpg?w=400&h=300&fit=fill'),
+    ('uI2kNFVKPnKZLMINKiNu8','Gregarious Giraffes','Giraffa','Pp28CuDqjVw','//images.ctfassets.net/utnfcxl3mjrn/4z0lTIf9FAhMvgsGm4raGk/608e8d506bcc23f4d48c52acb16cf37a/giraffe-DSC08632-1.jpg?w=400&h=300&fit=fill'),
+    ('3IwxzgIH7kNQcaCHt2sS2D','Watchful Ermines','Mustela erminea','6q1dIEkTBx4','//images.ctfassets.net/utnfcxl3mjrn/53p5Yl1RSYg3zQ4GGyPuBh/b491e53763686560a27c89db18202635/d2ba3js-c5db29db-e72b-437e-8805-3f49a662d1c4.jpg?w=400&h=300&fit=fill'),
+    ('2pJOCFJWW5gAkJtkfSogem','Outerworld Blue Sea Dragon','Glaucus atlanticus','sWztyHHIsFk','//images.ctfassets.net/utnfcxl3mjrn/4A8BJ9CkItNrL9QxY2MZfM/81de7cd0eaf0c8914e8ab8fe305cccdd/kmnoncgghdv.jpeg?w=400&h=300&fit=fill'),
+    ('z1M7sloUav65wz6aKPYip','Snooty Alpaca','Vicugna pacos','OUMH_1qxV0A','//images.ctfassets.net/utnfcxl3mjrn/2GSta0sqIr4PRIUpfCTUBe/5ad21d4f3ddb931ad1e2e8415d8e84f3/axMVEPL_460s.jpg?w=400&h=300&fit=fill'),
+    ('78e96W9TXkpNp8A376Z8S9','Gargantuan Giraffe weevils','Trachelophorus giraffa','2RMHXl4i4O0','//images.ctfassets.net/utnfcxl3mjrn/6017CgVoKvUlHPraT225fu/6cfb52fb1ff0c6b854f032bdaaa35ade/26916773_1508919934-768x432.jpg?w=400&h=300&fit=fill'),
+    ('2Z0HBpcHeua6M7VTZIe4EZ','Hilarious Hamsters','Cricetulus migratorius','rPLiGyWy5bQ','//images.ctfassets.net/utnfcxl3mjrn/7A4gcNij3ctVMWEC5KyAZ3/9406390d98052c578b4090c3a97ceee9/Goldhamster_terrarium.jpg?w=400&h=300&fit=fill'),
+    ('530W1J2Uz4exO3c9O4NSq1','Tantalizing Tardigrades','Milnesium tardigradum','eXBkmLzBHZk','//images.ctfassets.net/utnfcxl3mjrn/zzr3tUsUX7OPrDiJNzxPx/1910652dd3d021d721e1e24931213b8f/3511.jpg?w=400&h=300&fit=fill');
